2c134dc482fecc0e969cde9108e955ee661e217a
유인호 <yuiop1029@naver.com>
2024-04-18 23:30:04 +0900

    [UPDATE] Update EventsView

--- DevCommunity/Model/Decoding/PostsResultModel.swift
+++ DevCommunity/Model/Decoding/PostsResultModel.swift
@@ -7,58 +7,87 @@
 
 import Foundation
 
-struct PostsResultModel: Codable {
-	 let data: [Datum]
-	 let nextCursor: String
+struct EventPostsResultModel: Codable {
+	var data: [Datum]
+	let nextCursor: String
 
-	 enum CodingKeys: String, CodingKey {
-		  case data = "data"
-		  case nextCursor = "next_cursor"
-	 }
+	enum CodingKeys: String, CodingKey {
+		case data = "data"
+		case nextCursor = "next_cursor"
+	}
 }
 
 struct Datum: Codable {
-	 let postID: String
-	 let productID: String?
-	 let content: String
-	 let content1: String?
-	 let content2: String?
-	 let content3: String?
-	 let content4: String?
-	 let createdAt: String
-	 let creator: Creator
-	 let files: [String]
-	 let likes: [String]
-	 let likes2: [String]
-	 let hashTags: [String]
-	 let comments: [String]
-	 let title: String?
+	let postID: String
+	let productID: String
+	private let content: String
+	private let content1: String
+	private let content2: String
+	let organizer: String
+	let url: String
+	let recruitProductId: String
+	let createdAt: String
+	let creator: Creator
+	let files: [String]
+	private let likes: [String]
+	private let likes2: [String]
+	let hashTags: [String]
+	let title: String
 
-	 enum CodingKeys: String, CodingKey {
-		  case postID = "post_id"
-		  case productID = "product_id"
-		  case content = "content"
-		  case content1 = "content1"
-		  case content2 = "content2"
-		  case content3 = "content3"
-		  case content4 = "content4"
-		  case createdAt = "createdAt"
-		  case creator = "creator"
-		  case files = "files"
-		  case likes = "likes"
-		  case likes2 = "likes2"
-		  case hashTags = "hashTags"
-		  case comments = "comments"
-		  case title = "title"
-	 }
+	var dateStart: Date {
+		let formatter = DateFormatter()
+		return formatter.date(from: content1) ?? Date()
+	}
+
+	var dateEnd: Date {
+		let formatter = DateFormatter()
+		return formatter.date(from: content2) ?? Date()
+	}
+
+	enum CodingKeys: String, CodingKey {
+		case postID = "post_id"
+		case productID = "product_id"
+		case content = "content"
+		case content1 = "content1"
+		case content2 = "content2"
+		case organizer = "content3"
+		case url = "content4"
+		case recruitProductId = "content5"
+		case createdAt = "createdAt"
+		case creator = "creator"
+		case files = "files"
+		case likes = "likes"
+		case likes2 = "likes2"
+		case hashTags = "hashTags"
+		case title = "title"
+	}
+
+	init(from decoder: any Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		self.postID = try container.decodeIfPresent(String.self, forKey: .postID) ?? ""
+		self.productID = try container.decodeIfPresent(String.self, forKey: .productID) ?? ""
+		self.content = try container.decodeIfPresent(String.self, forKey: .content) ?? ""
+		self.content1 = try container.decodeIfPresent(String.self, forKey: .content1) ?? ""
+		self.content2 = try container.decodeIfPresent(String.self, forKey: .content2) ?? ""
+		self.organizer = try container.decodeIfPresent(String.self, forKey: .organizer) ?? ""
+		self.url = try container.decodeIfPresent(String.self, forKey: .url) ?? ""
+		self.recruitProductId = try container.decodeIfPresent(String.self, forKey: .recruitProductId) ?? ""
+		self.createdAt = try container.decodeIfPresent(String.self, forKey: .createdAt) ?? ""
+		self.creator = try container.decodeIfPresent(Creator.self, forKey: .creator) ?? Creator(userID: "", nick: "")
+		self.files = try container.decodeIfPresent([String].self, forKey: .files) ?? []
+		self.likes = try container.decodeIfPresent([String].self, forKey: .likes) ?? []
+		self.likes2 = try container.decodeIfPresent([String].self, forKey: .likes2) ?? []
+		self.hashTags = try container.decodeIfPresent([String].self, forKey: .hashTags) ?? []
+		self.title = try container.decodeIfPresent(String.self, forKey: .title) ?? ""
+	}
 }
 
 struct Creator: Codable {
-	 let userID: String
-	 let nick: String
+	let userID: String
+	let nick: String
 
-	 enum CodingKeys: String, CodingKey {
-		  case userID = "user_id"
-		  case nick = "nick"
-	 }
+	enum CodingKeys: String, CodingKey {
+		case userID = "user_id"
+		case nick = "nick"
+	}
 }
--- DevCommunity/Scene/EventsScene/View/EventsView.swift
+++ DevCommunity/Scene/EventsScene/View/EventsView.swift
@@ -9,20 +9,15 @@ import UIKit
 import SnapKit
 
 final class EventsView: BaseUIView {
-	let testLabel = UILabel()
 
 	override func configureHierarchy() {
-		[testLabel].forEach { addSubview($0) }
+//		[].forEach { addSubview($0) }
 	}
 
 	override func configureLayout() {
-		testLabel.snp.makeConstraints {
-			$0.center.equalTo(self)
-			$0.height.equalTo(30)
-		}
+
 	}
 
 	override func configureView() {
-		testLabel.text = "Main"
 	}
 }
