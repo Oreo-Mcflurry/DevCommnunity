7903a4386aed73fe5ee4ddc19f8c438830beafad
유인호 <yuiop1029@naver.com>
2024-05-04 22:00:34 +0900

    [UPDATE] SettingView UI

--- DevCommunity/Scene/SettingScene/View/SettingView.swift
+++ DevCommunity/Scene/SettingScene/View/SettingView.swift
@@ -5,9 +5,77 @@
 //  Created by A_Mcflurry on 4/17/24.
 //
 
-import UIKit
-import SnapKit
+import SwiftUI
 
-final class SettingView: BaseUIView {
-	
+struct SettingView: View {
+	var body: some View {
+		List {
+			Section {
+				NavigationLink {
+
+				} label: {
+					Label(
+						title: { Text("프로필 관리") },
+						icon: { Image(systemName: "person.fill").foregroundStyle(.green) }
+					)
+				}
+
+				NavigationLink {
+
+				} label: {
+					Label(
+						title: { Text("계정 관리") },
+						icon: { Image(systemName: "key.fill").foregroundStyle(.yellow) }
+					)
+				}
+
+			} header: {
+				Text("계정")
+			}
+
+			Section {
+				NavigationLink {
+
+				} label: {
+					Label(
+						title: { Text("문의하기") },
+						icon: { Image(systemName: "headphones").foregroundStyle(.indigo) }
+					)
+				}
+
+				NavigationLink {
+
+				} label: {
+					Label(
+						title: { Text("이용 약관") },
+						icon: { Image(systemName: "pencil").foregroundStyle(.red) }
+					)
+				}
+
+				NavigationLink {
+
+				} label: {
+					Label(
+						title: { Text("개인정보 처리방침") },
+						icon: { Image(systemName: "hand.raised.fill").foregroundStyle(.orange) }
+					)
+				}
+
+			} header: {
+				Text("서비스 이용")
+			}
+		}
+		.navigationTitle("설정")
+		.navigationBarTitleDisplayMode(.inline)
+		.listStyle(.plain)
+		.background(.white)
+		.headerProminence(.increased)
+
+	}
+}
+
+#Preview {
+	NavigationStack {
+		SettingView()
+	}
 }
