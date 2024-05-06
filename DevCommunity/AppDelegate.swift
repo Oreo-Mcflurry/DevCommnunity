6872384a319dda63f38646d2c817be4f54fa7fc9
유인호 <yuiop1029@naver.com>
2024-05-06 22:45:18 +0900

    [ADD] Add Portone SDK

--- DevCommunity/AppDelegate.swift
+++ DevCommunity/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Kingfisher
 import IQKeyboardManagerSwift
+import iamport_ios
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		IQKeyboardManager.shared.enable = true
+
 		return true
 	}
 
@@ -31,6 +33,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the user discards a scene session.
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	}
+
+	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+		Iamport.shared.receivedURL(url)
+		return true
 	}
 
 
